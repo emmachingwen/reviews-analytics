d0704efdff46e34afdb170f25278f35ada206dd8
你的名字 <>
2024-05-06 14:16:31 +0800

    calc avg reviews

--- read.py
+++ read.py
@@ -3,11 +3,13 @@ count = 0
 with open('reviews.txt' , 'r') as f:
 	for line in f:
 		data.append(line)  
-		count += 1
+		count += 1  #count=count+1
 		if count % 1000 == 0:  # %是求餘數的意思
 			print(len(data)) 
-print(len(data))
-print(data[0])
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print('---------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言平均長度為', sum_len/len(data))
