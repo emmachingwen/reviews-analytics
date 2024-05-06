6cd3af990f276817ebc8043bab9ee8f82879e060
你的名字 <>
2024-05-06 14:33:04 +0800

    add filter

--- read.py
+++ read.py
@@ -13,3 +13,11 @@ for d in data:
 	sum_len = sum_len + len(d)
 
 print('留言平均長度為', sum_len/len(data))
+
+#篩選
+new = []
+for d in data:  #意思就是把data清單裡面的東西一個一個拿出來，都叫d
+	if len(d) < 100:
+		new.append(d)  #長度小於100的都裝進新的清單裡 
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0]) #印出new清單裡的第一筆
